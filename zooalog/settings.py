d2b00298070fdec3bc964e2c01b92950ef41f6a3
emilyaikens <emily.c.aikens@gmail.com>
2022-12-06 12:07:07 -0800

    remove hardcode django secret key

--- zooalog/settings.py
+++ zooalog/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h56(bsvb+oy+7jxji5k47&z3n4901(wlnhym)j=$8!)=fbak9k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
