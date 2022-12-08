db5548d010e53e3588916f90d6c642a748b83e49
emilyaikens <emily.c.aikens@gmail.com>
2022-12-07 18:38:02 -0800

    add number to Test class and add loop with conditionals to templates/test/index

--- main_app/views.py
+++ main_app/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 class Test:
-    def __init__(self, name, description):
+    def __init__(self, name, description, number):
         self.name = name
         self.description = description
 
 tests = [
-    Test('one', 'first test'),
-    Test('two', 'second test')
+    Test('one', 'first test', 3),
+    Test('two', 'second test', 10),
 ]
 
 # View functions must define a positional parameter to accept a request object
