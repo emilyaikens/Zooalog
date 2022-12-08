74388d08b3da13f339e26635deb4a4fa31dc155b
emilyaikens <emily.c.aikens@gmail.com>
2022-12-07 18:34:33 -0800

    add test class and dictionary to views to test data handling

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Test:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+tests = [
+    Test('one', 'first test'),
+    Test('two', 'second test')
+]
+
 # View functions must define a positional parameter to accept a request object
 # Third positional argument is a dictionary
 
@@ -17,4 +27,4 @@ def about(request):
 
 # Define the test view
 def test_index(request):
-    return render(request, 'test/index.html', { 'test': test })
+    return render(request, 'test/index.html', { 'tests': tests })
