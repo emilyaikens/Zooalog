f972cbd5e22c0afcd8c6b2cb841759b08823b3e9
emilyaikens <emily.c.aikens@gmail.com>
2022-12-08 18:17:01 -0800

    add Update and Delete CBVs to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 # import path from zooalog/urls.py
 # the path function is used to define each route
 # CBV(class based view) ListView, DetailView, CreateView, DeleteView and UpdateView
+# CBVs require a int:pk, rather than int:test_id
 from django.urls import path
 from . import views
 
@@ -9,5 +10,7 @@ urlpatterns = [
     path('about', views.about, name="about"),
     path('test/', views.test_index, name='index'),
     path('test/<int:test_id>/', views.test_detail, name='detail'),
-    path('test/create/', views.TestCreate.as_view(), name='test_create')
+    path('test/create/', views.TestCreate.as_view(), name='test_create'),
+    path('test/<int:pk>/update/', views.TestUpdate.as_view(), name="test_update"),
+    path('test/<int:pk>/delete/', views.TestDelete.as_view(), name="test_delete"),
 ]
