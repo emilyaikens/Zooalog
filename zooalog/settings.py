407616a897bac9b6939898083b85daf35cf64de5
emilyaikens <emily.c.aikens@gmail.com>
2022-12-11 17:15:43 -0800

    add login redirect to settings

--- zooalog/settings.py
+++ zooalog/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 
 STATIC_URL = '/main_app/static/'
 
+LOGIN_REDIRECT_URL = '/cats/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
