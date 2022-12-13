ce51ca23d01ed70edcdd17ad22fc03a836845f9c
emilyaikens <emily.c.aikens@gmail.com>
2022-12-13 11:14:24 -0800

    update admin.py with new model names

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Test, Sub
+from .models import Enclosure, Animal
 
 # Models registered here
-admin.site.register(Test)
-admin.site.register(Sub)
+admin.site.register(Enclosure)
+admin.site.register(Animal)
