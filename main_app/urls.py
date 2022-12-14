b60597aedcc28d01995820b568383ae958e58568
emilyaikens <emily.c.aikens@gmail.com>
2022-12-14 12:17:00 -0800

    path update animal

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,15 +8,20 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about', views.about, name="about"),
+
     path('enclosures/', views.enclosure_index, name='index'),
     path('enclosures/<int:enclosure_id>/', views.enclosure_detail, name='detail'),
     path('enclosures/create/', views.EnclosureCreate.as_view(), name='enclosure_create'),
     path('enclosures/<int:pk>/update/', views.EnclosureUpdate.as_view(), name="enclosure_update"),
     path('enclosures/<int:pk>/delete/', views.EnclosureDelete.as_view(), name="enclosure_delete"), 
+
     path('animals/<int:enclosure_id>/add_animal', views.add_animal, name='add_animal'),
     path('animals/<int:enclosure_id>/create_animal/', views.create_animal, name='create_animal'),
+    path('animals/<int:animal_id>/update/', views.update_animal, name='update_animal'),
+
     path('parameters/<int:enclosure_id>/add_parameter', views.add_parameter, name='add_parameter'),
     path('parameters/<int:enclosure_id>/create_parameter/', views.create_parameter, name='create_parameter'),
+
     path('parameters/info', views.parameter_info, name='parameter_info'),
     path('accounts/signup', views.signup, name='signup'),
     ]
