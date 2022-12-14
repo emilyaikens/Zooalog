e76c576fb4618d5131c1c3fa164b0c35bb5fd3b5
emilyaikens <emily.c.aikens@gmail.com>
2022-12-14 11:01:36 -0800

    add not-required to fields in forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -11,6 +11,13 @@ class AnimalForm(ModelForm):
             'quantity'
         ]
 
+    def __init__(self, *args, **kwargs):
+        super(AnimalForm, self).__init__(*args, **kwargs)
+        self.fields['given_name'].required = False
+        self.fields['scientific_name'].required = False
+        self.fields['quantity'].required = False
+
+
 class ParameterForm(ModelForm):
     class Meta:
         model = Parameter
@@ -21,3 +28,9 @@ class ParameterForm(ModelForm):
             'frequency',
             'notes'
         ]
+    
+    def __init__(self, *args, **kwargs):
+        super(ParameterForm, self).__init__(*args, **kwargs)
+        self.fields['ideal_range'].required = False
+        self.fields['frequency'].required = False
+        self.fields['notes'].required = False
