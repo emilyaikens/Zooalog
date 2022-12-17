5dcd533a3f7fee99176f47a13b0a437a836d455b
emilyaikens <emily.c.aikens@gmail.com>
2022-12-16 18:57:41 -0800

    small fix - change enclosure type display in index.html

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,7 @@
 
 from django.db import models
 from django.urls import reverse
-from datetime import date
+from datetime import date, time
 from django.contrib.auth.models import User
 
 TYPES = (
@@ -50,7 +50,7 @@ class Animal(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class Parameter(models.Model):
     parameter = models.CharField(max_length=250)
@@ -66,5 +66,18 @@ class Parameter(models.Model):
     def __str__(self):
         return self.name
 
-        # class Meta:
-    #     ordering = ['-date']
+
+class ParameterLog(models.Model):
+    date = models.DateField()
+    time = models.TimeField(blank = True)
+
+    # Create foreign key for Enclosure and Parameter
+    # on_delete: when an enclosure is deleted, all of its children will also be deleted
+    enclosure = models.ForeignKey(Enclosure, on_delete=models.CASCADE)
+    parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-date']
